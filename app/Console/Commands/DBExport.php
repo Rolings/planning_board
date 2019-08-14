71f73fccc7b7e0ca4618ac0ddfbb2b0ecd509461
Dmitriy Shramko <dmitriy.sh.itlt@gmail.com>
2019-08-14 18:34:23 +0300

    Add command create backup

--- app/Console/Commands/DBExport.php
+++ app/Console/Commands/DBExport.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
-class DumpGenerate extends Command
+class DBExport extends Command
 {
     /**
      * The name and signature of the console command.
--- app/Console/Commands/DBImport.php
+++ app/Console/Commands/DBImport.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class DbImport extends Command
+class DBImport extends Command
 {
     /**
      * The name and signature of the console command.
